f1c8feadb98ac823331a168dbf54494483fd600c
Miles Smith <milesdavidesmith@gmail.com>
2022-07-23 13:45:59 -0700

    new binding way

--- cpu_state.h
+++ cpu_state.h
@@ -5,6 +5,7 @@
 #include "mmu.h"
 #include "register.h"
 
+#include <functional>
 #include <queue>
 
 #define OPCODE(inst) (((inst) >> 9) & 0x007f)
@@ -15,6 +16,7 @@
 #define REGISTER_OP(opcode, op)                                                \
     printf("register opcode %s to op %s\n", #opcode, #op);                     \
     opcode_mapping_.at((opcode) >> 1) = (op)
+#define BIND_OP(op) (std::bind(op, this))
 
 namespace MPCE
 {
@@ -49,68 +51,77 @@ struct CPUState
     Register<uint8_t> mode_{"mode", 0xfe};
 
   public:
-    CPUState() : opcode_mapping_{OPCODE_MAP_SIZE, op_alu(0, false)}
+    CPUState() : opcode_mapping_{OPCODE_MAP_SIZE, BIND_OP(&CPUState::op_none)}
     {
-        // Todo: define all these.
-        REGISTER_OP(0x24, op_alu(1, false));
-        REGISTER_OP(0xc4, op_alu(2, false));
-        REGISTER_OP(0x26, op_alu(3, false));
-        REGISTER_OP(0x2a, op_alu(4, false));
-        REGISTER_OP(0x2c, op_alu(5, false));
-        REGISTER_OP(0xcc, op_alu(6, false));
+        // Opcodes appear as multiples of 2 because they are the top 7 bits of
+        // the top byte of the instruction word. The lowest bit in the byte is
+        // always zero.
+        //
+        // Syntax quirk: Due to the preprocessor misinterpreting the comma,
+        // wrap the macro argument in extra parentheses if it is a function
+        // templatized on multiple template parameters.
 
-        REGISTER_OP(0x32, op_alu(0, false));
-        REGISTER_OP(0x34, op_alu(0, false));
-        REGISTER_OP(0x36, op_alu(0, false));
-        REGISTER_OP(0x3a, op_alu(0, false));
-        REGISTER_OP(0x3c, op_alu(0, false));
+        // Arithmetic:
 
-        REGISTER_OP(0xb2, op_alu(0, false));
-        REGISTER_OP(0xb4, op_alu(0, false));
-        REGISTER_OP(0xb4, op_alu(0, false));
-        REGISTER_OP(0xb6, op_alu(0, false));
-        REGISTER_OP(0xb8, op_alu(0, false));
-        REGISTER_OP(0xba, op_alu(0, false));
-        REGISTER_OP(0xbc, op_alu(0, false));
+        // 00   noop
+        // 22   x <- y ^ z
+        REGISTER_OP(0x22, BIND_OP((&CPUState::op_alu<0, false, false>)));
 
-        REGISTER_OP(0x42, op_alu(0, false));
-        REGISTER_OP(0x44, op_alu(0, false));
-        REGISTER_OP(0x46, op_alu(0, false));
-        REGISTER_OP(0x48, op_alu(0, false));
+        // 24   x <- y - z
+        REGISTER_OP(0x24, BIND_OP((&CPUState::op_alu<1, false, false>)));
 
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4c, op_alu(0, false));
-        REGISTER_OP(0x4e, op_alu(0, false));
-        REGISTER_OP(0x84, op_alu(0, false));
+        // c4   x <- y - z, carry
+        REGISTER_OP(0xc4, BIND_OP((&CPUState::op_alu<1, true, false>)));
 
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
+        // 26   x <- y & z
+        REGISTER_OP(0x26, BIND_OP((&CPUState::op_alu<2, false, false>)));
 
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
+        // 2a   x <- y | z
+        REGISTER_OP(0x2a, BIND_OP((&CPUState::op_alu<3, false, false>)));
 
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
-        REGISTER_OP(0x4a, op_alu(0, false));
+        // 2c   x <- y + z
+        REGISTER_OP(0x2c, BIND_OP((&CPUState::op_alu<4, false, false>)));
+
+        // cc   x <- y + z, carry
+        REGISTER_OP(0xcc, BIND_OP((&CPUState::op_alu<4, true, false>)));
+
+        // Arithmetic with immediate:
+        // Todo: Is carry really not needed with immediate?
+
+        // 32   x <- y ^ z, imm
+        REGISTER_OP(0x32, BIND_OP((&CPUState::op_alu<0, false, true>)));
+
+        // 34   x <- y - z, imm
+        REGISTER_OP(0x34, BIND_OP((&CPUState::op_alu<1, false, true>)));
+
+        // 36   x <- y & z, imm
+        REGISTER_OP(0x36, BIND_OP((&CPUState::op_alu<2, false, true>)));
+
+        // 3a   x <- y | z, imm
+        REGISTER_OP(0x3a, BIND_OP((&CPUState::op_alu<3, false, true>)));
+
+        // 3c   x <- y + z, imm
+        REGISTER_OP(0x3a, BIND_OP((&CPUState::op_alu<4, false, true>)));
+
+        // b2   mem_b[y + z] <- x
+        REGISTER_OP(0x3a,
+                    BIND_OP((&CPUState::op_mem<true, false, true, true>)));
+
+        // b4   mem_b[y + z] <- x, imm
+        // b6   x <- mem_bu[y + z]
+        // b8   x <- mem_bu[y + z], imm
+        // ba   x <- mem_bs[y + z]
+        // bc   x <- mem_bs[y + z], imm
+
+        // 42   mem_w[y + z] <- x
+        // 44   mem_w[y + z] <- x, imm
+        // 46   x <- mem_w[y + z]
+        // 48   x <- mem_w[y + z], imm
+
+        // 4a   mem_t[y + z] <- x
+        // 4c   mem_t[y + z] <- x, imm
+        // 4e   x <- mem_t[y + z]
+        // 84   x <- mem_t[y + z], imm
     }
 
     void cycle()
@@ -119,7 +130,7 @@ struct CPUState
 
         const uint16_t inst_word = inst_.read();
         const Operation operation = opcode_mapping_[OPCODE(inst_word)];
-
+        cout << "opcode is " << (int)inst_word << endl;
         // Perform the operation.
         operation();
 
@@ -150,150 +161,137 @@ struct CPUState
         inst_.write(inst_word);
     }
 
-    Operation op_alu(uint32_t alu_sel, bool carry_in)
+    template <uint32_t N> void f()
     {
-        return [&, alu_sel, carry_in]() {
-            cout << "performing alu op " << alu_sel << ", carry_in " << carry_in
-                 << endl;
-            const uint16_t inst_word = inst_.read();
-
-            Register<uint16_t> &reg_x =
-                register_file_.get(REG_SEL_X(inst_word));
-            Register<uint16_t> &reg_y =
-                register_file_.get(REG_SEL_Y(inst_word));
-            Register<uint16_t> &reg_z =
-                register_file_.get(REG_SEL_Z(inst_word));
-
-            uint16_t x = 0;
-            uint16_t y = reg_y.read();
-            uint16_t z = reg_z.read();
-
-            bool update_status = false;
-            bool update_status_unsigned = false;
-            bool carry_out = false;
-            bool overflow = false;
-            bool zero = false;
-            bool negative = false;
-
-            switch (alu_sel)
-            {
-            case 0:
-                // Addition.
-                x = static_cast<uint16_t>(static_cast<int16_t>(y) +
-                                          static_cast<int16_t>(z));
-                update_status = true;
-                break;
-            default:
-                // Subtraction.
-                x = static_cast<uint16_t>(static_cast<int16_t>(y) -
-                                          static_cast<int16_t>(z));
-                update_status = true;
-                break;
-            }
-
-            reg_z.write(z);
-
-            if (update_status)
-            {
-                negative = static_cast<int16_t>(z) < 0;
-            }
-
-            if (update_status || update_status_unsigned)
-            {
-                zero = z == 0;
-                // Todo: Set carry_out, overflow.
-            }
-        };
+        cout << "dandy\n";
     }
 
-    Operation op_mem_data_load(bool byte, bool mode)
+    /// @tparam alu_sel
+    /// @tparam carry_in
+    /// @tparam imm
+    template <uint32_t alu_sel, bool carry_in, bool imm> void op_alu()
     {
-        return [&, byte, mode]() {
-            cout << "performing mem data load, byte = " << byte << endl;
+        cout << "performing alu op " << alu_sel << ", carry_in " << carry_in
+             << endl;
+        const uint16_t inst_word = inst_.read();
 
-            if (!is_valid_mode(mode))
-            {
-                // Todo: Generate interrupt.
-                return;
-            }
+        Register<uint16_t> &reg_x = register_file_.get(REG_SEL_X(inst_word));
+        Register<uint16_t> &reg_y = register_file_.get(REG_SEL_Y(inst_word));
+        Register<uint16_t> &reg_z = register_file_.get(REG_SEL_Z(inst_word));
 
-            const uint16_t inst_word = inst_.read();
+        uint16_t x = 0;
+        uint16_t y = reg_y.read();
+        uint16_t z = reg_z.read();
 
-            Register<uint16_t> &reg_x =
-                register_file_.get(REG_SEL_X(inst_word));
+        bool update_status = false;
+        bool update_status_unsigned = false;
+        bool carry_out = false;
+        bool overflow = false;
+        bool zero = false;
+        bool negative = false;
 
-            const uint16_t y = register_file_.get(REG_SEL_Y(inst_word)).read();
-            const uint16_t z = register_file_.get(REG_SEL_Z(inst_word)).read();
-            const uint16_t virt_addr = y + z;
+        switch (alu_sel)
+        {
+        case 0:
+            // Addition.
+            x = static_cast<uint16_t>(static_cast<int16_t>(y) +
+                                      static_cast<int16_t>(z));
+            update_status = true;
+            break;
+        default:
+            // Subtraction.
+            x = static_cast<uint16_t>(static_cast<int16_t>(y) -
+                                      static_cast<int16_t>(z));
+            update_status = true;
+            break;
+        }
 
-            mmu_.reset_fault();
+        reg_z.write(z);
 
-            const uint32_t phys_addr =
-                is_user_mode()
-                    ? mmu_.resolve(virt_addr, ptb_.read(), true, true, false)
-                    : virt_addr;
+        if (update_status)
+        {
+            negative = static_cast<int16_t>(z) < 0;
+        }
 
-            if (mmu_.page_fault())
-            {
-                // Todo: Generate interrupt and return in case of faults.
-            }
+        if (update_status || update_status_unsigned)
+        {
+            zero = z == 0;
+            // Todo: Set carry_out, overflow.
+        }
+    }
 
+    /// @tparam byte
+    /// @tparam mode
+    /// @tparam is_data
+    /// @tparam write
+    template <bool byte, bool mode, bool is_data, bool write> void op_mem()
+    {
+        cout << "performing mem data load, byte = " << byte << endl;
+
+        if (!is_valid_mode(mode))
+        {
+            // Todo: Generate interrupt.
+            return;
+        }
+
+        const uint16_t inst_word = inst_.read();
+
+        Register<uint16_t> &reg_x = register_file_.get(REG_SEL_X(inst_word));
+
+        const uint16_t y = register_file_.get(REG_SEL_Y(inst_word)).read();
+        const uint16_t z = register_file_.get(REG_SEL_Z(inst_word)).read();
+
+        mmu_.reset_fault();
+
+        const uint16_t virt_addr = y + z;
+        const uint32_t phys_addr =
+            is_user_mode()
+                ? mmu_.resolve(virt_addr, ptb_.read(), true, true, false)
+                : virt_addr;
+
+        if (mmu_.page_fault() || write && mmu_.read_only_fault())
+        {
+            // Todo: Generate interrupt and return in case of faults.
+        }
+
+        // Todo: make ByteAddressibleMemory and WordAddressibleMemory satisfy a
+        // Memory interface, where WordAddressibleMemory ignores the byte
+        // parameter. A lot of this control flow can be factored out that way.
+        if (is_data)
+        {
             const ByteAddressibleMemory &data = mmio_.get_data(mode);
-            reg_x.write(data.read(phys_addr, byte));
-        };
-    }
 
-    Operation op_mem_data_store(bool byte, bool mode)
-    {
-        return [&, byte, mode]() {
-            cout << "performing mem data store, byte = " << byte << endl;
-
-            if (!is_valid_mode(mode))
+            if (write)
             {
-                // Todo: Generate interrupt.
-                return;
-            }
-
-            const uint16_t inst_word = inst_.read();
-
-            const uint16_t x = register_file_.get(REG_SEL_X(inst_word)).read();
-            const uint16_t y = register_file_.get(REG_SEL_Y(inst_word)).read();
-            const uint16_t z = register_file_.get(REG_SEL_Z(inst_word)).read();
-            const uint16_t virt_addr = y + z;
-
-            mmu_.reset_fault();
-
-            const uint32_t phys_addr =
-                is_user_mode()
-                    ? mmu_.resolve(virt_addr, ptb_.read(), true, true, true)
-                    : virt_addr;
-
-            if (mmu_.page_fault())
-            {
-                // Todo: Generate interrupt and return in case of faults.
-            }
-            else if (mmu_.read_only_fault())
-            {
-                // Todo: Generate interrupt and return in case of faults.
-            }
-
-            ByteAddressibleMemory &data = mmio_.get_data(mode);
-
-            if (byte)
-            {
-                data.store_byte(phys_addr, static_cast<uint8_t>(x));
+                reg_x.write(data.read(phys_addr, byte));
             }
             else
             {
-                data.store_word(phys_addr, x);
+                data.store(phys_addr, reg_x.read(), byte);
             }
-        };
+        }
+        else
+        {
+            const WordAddressibleMemory &code = mmio_.get_code(mode);
+            if (write)
+            {
+                reg_x.write(code.read(phys_addr, byte));
+            }
+            else
+            {
+                code.store(phys_addr, reg_x.read(), byte);
+            }
+        }
     }
 
     void op_invalid()
     {
         cerr << "invalid operation." << endl;
         // Todo: Generate interrupt.
+    }
+
+    void op_none()
+    {
     }
 };
 } // namespace MPCE
--- main.cc
+++ main.cc
@@ -3,16 +3,10 @@
 
 #include <cstdio>
 
-int main()
+int main(int argc, char *argv[])
 {
     MPCE::CPUState cpu_state;
     cpu_state.cycle();
-
-    uint16_t u = 0x100a;
-
-    int16_t s = static_cast<int16_t>(u);
-
-    printf("%x\n", static_cast<int>(s));
 
     return 0;
 }
